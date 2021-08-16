9bb73a381783f3d2ec1bccffc331e56e73e5afd9
mathiasgheno <mathiasgheno@gmail.com>
2021-08-16 19:06:28 -0300

    chore: add retorno de arquivo html

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
 app.get('/', function (req, res) {
-    res.send('hello nadine')
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
 
 app.listen(80);
